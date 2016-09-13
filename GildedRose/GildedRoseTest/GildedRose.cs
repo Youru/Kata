52101900f32347d93d742becba36edd1faad4af9
Uru <johann.vanbeveren@gmail.com>
2016-09-13 21:26:52 +0200

    Init Kata GildedRose

---
+++ GildedRose/GildedRoseTest/GildedRose.cs
@@ -0,0 +1,20 @@
+﻿using GildedRose;
+using System;
+using System.Collections.Generic;
+using Xunit;
+using NFluent;
+namespace GildedRoseTest
+{
+    public class GildedRose
+    {
+        [Fact]
+        public void foo()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
+            GildedRoseService app = new GildedRoseService(Items);
+            app.UpdateQuality();
+            Check.That(Items[0].Name).Equals("foo");
+        }
+
+    }
+}
