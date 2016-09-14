04f484de7cf2b3e897e302904c0f332ba311dcff
Uru <johann.vanbeveren@gmail.com>
2016-09-14 15:10:11 +0200

    First pull Diamond

---
+++ Diamond/Diamond/DiamondService.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Diamond
+{
+    public class DiamondService
+    {
+        string _alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        public List<string> CreateListDiamond(char letter)
+        {
+            var lineHalfList =  GetHalfList(letter);
+            var lineFullList = GetFullList(lineHalfList, _alphabet.IndexOf(letter));
+         
+            return lineFullList;
+        }
+
+        public string CreateStringDiamond(char letter)
+        {
+            var stringDiamond = new StringBuilder();
+            var listdiamond = CreateListDiamond(letter);
+            listdiamond.ForEach(l => stringDiamond.AppendLine(l));
+
+            return stringDiamond.ToString();
+        }
+
+        private List<String> GetHalfList(char letter)
+        {
+            var lineHalfList = new List<String>();
+            
+            for (int i= 0; i<=_alphabet.IndexOf(letter);i++)
+            {
+                lineHalfList.Add(DiamondLine(_alphabet[i], _alphabet.IndexOf(letter)));
+            }
+
+            return lineHalfList;
+        }
+
+        private List<string> GetFullList(List<string> lineHalfList, int indexOfLetter)
+        {
+            var list = new List<string>(lineHalfList);
+            lineHalfList.Reverse();
+            if (lineHalfList.Count > 1) lineHalfList.Remove(lineHalfList.First());
+            list.AddRange(lineHalfList);
+
+            return list;
+        }
+
+        private string DiamondLine(char letter, int index)
+        {
+            var indexOfLetter = _alphabet.IndexOf(letter);
+            var outerSpace = GetSpace(index - indexOfLetter);
+            var innerSpace = GetSpace(2 * (indexOfLetter) - 1);
+
+            return outerSpace + letter + innerSpace + ((letter == 'A')?"":letter.ToString());
+        }
+
+        private string GetSpace(int index)
+        {
+            if (index < 0) index = 0;
+            
+            return "".PadRight(index, ' ');
+        }
+    }
+}
---
+++ Diamond/DiamondTest/Diamond.cs
@@ -0,0 +1,68 @@
+﻿using Diamond;
+using NFluent;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+namespace DiamondTest
+{
+    public class Diamond
+    {
+        private DiamondService service;
+        public Diamond()
+        {
+            service = new DiamondService();
+        }
+
+        [Fact]
+        public void Should_Contain_2N_Line_When_Only_Letter_A()
+        {
+            var result = service.CreateListDiamond('A');
+
+            Check.That(result.Count).Equals(2);
+        }
+
+        [Fact]
+        public void Should_Contain_2N_Minus_1_Line_When_Other_Letter_Than_A()
+        {
+            var result = service.CreateListDiamond('B');
+
+            Check.That(result.Count).Equals(3);
+        }
+
+        [Fact]
+        public void Should_Contain_2N_Minus_1_Inner_Space_When_Ohter_Letter_Than_A()
+        {
+            var result = service.CreateListDiamond('C');
+
+            Check.That(result.FirstOrDefault(r=>r.Contains("C"))).Contains("   ");
+        }
+
+        [Fact]
+        public void Should_Have_The_Same_Result_When_Reversed()
+        {
+            var listDiamond = new List<string>();
+            var result = service.CreateListDiamond('Y');
+            listDiamond = result;
+            result.Reverse();
+            Check.That(result).Equals(listDiamond);
+        }
+
+        [Fact]
+        public void Should_Have_The_Expected_Result()
+        {
+            StringBuilder resultExpected = new StringBuilder();
+            resultExpected.AppendLine("  A");
+            resultExpected.AppendLine(" B B");
+            resultExpected.AppendLine("C   C");
+            resultExpected.AppendLine(" B B");
+            resultExpected.AppendLine("  A");
+
+            var result = service.CreateStringDiamond('C');
+
+            Check.That(result).Equals(resultExpected.ToString());
+        }
+    }
+}
